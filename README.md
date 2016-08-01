2becf238f4047f5e3061d2893b8f425902fdae47
elekt <elekestamas@protonmail.com>
2016-08-01 18:44:57 +0200

    Writing a text based dream entry also bypass lock screen. Updated README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Dream-Diary-Alarm-Clock
+
+This Android project is written purely in Kotlin. A simple alarm clock, with theextra feature of offering to record, or write your dream.
+
+A dream diary (or dream journal) is a diary in which dream experiences are recorded. A dream diary might include a record of nightly dreams, personal reflections and waking dream experiences. It is often used in the study of dreams and psychology. Dream diaries are also used by some people as a way to help induce lucid dreams. <b>They are also regarded as a useful catalyst for remembering dreams.</b>
+
--- app/src/main/kotlin/com/elekt/dreamcatcher/activities/WriteDreamActivity.kt
+++ app/src/main/kotlin/com/elekt/dreamcatcher/activities/WriteDreamActivity.kt
@@ -1,7 +1,8 @@
 package com.elekt.dreamcatcher.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 import com.elekt.dreamcatcher.DIRECTORY
 import com.elekt.dreamcatcher.R
 import kotlinx.android.synthetic.main.write_dream_activity.*
@@ -16,6 +17,8 @@ class WriteDreamActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.write_dream_activity)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD)
 
         val dirPath = intent.getStringExtra(DIRECTORY)
         val timestamp = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", Locale.getDefault()).format(Calendar.getInstance().time)
